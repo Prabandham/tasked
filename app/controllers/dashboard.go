b8302106a4e9de15efb1cab0953f7f257b6a1221
Icicle technologies <srinidhi.prabandham@icicletech.com>
2017-03-30 16:10:50 +0530

    removing loading user in dashboard

--- app/controllers/dashboard.go
+++ app/controllers/dashboard.go
@@ -14,12 +14,12 @@ type Dashboard struct {
 }
 
 func (c Dashboard) Index() revel.Result {
-	var user models.User
-	app.DB.First(&user, c.Session["user_id"])
+	//var user models.User
+	//app.DB.First(&user, c.Session["user_id"])
 
 	var projects []models.Project
 
-	query := fmt.Sprintf("SELECT * FROM projects INNER JOIN user_projects ON user_projects.project_id = projects.id WHERE user_projects.user_id = %d", user.ID)
+	query := fmt.Sprintf("SELECT * FROM projects INNER JOIN user_projects ON user_projects.project_id = projects.id WHERE user_projects.user_id = %s", c.Session["user_id"])
 	app.DB.Raw(query).Scan(&projects)
 	projectsCount := len(projects)
 
